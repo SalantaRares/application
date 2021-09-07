1587dcddd5d9d17053ae82c2a63bd288dc471fde
sergiu.maris <sergiu.maris@btrl.ro>
2021-09-07 17:23:47 +0300

    initial commit

---
+++ src/main/java/ro/btrl/miswebappspringdemo/MisWebappSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package ro.btrl.miswebappspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisWebappSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisWebappSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/btrl/miswebappspringdemo/MisWebappSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.btrl.miswebappspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisWebappSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
