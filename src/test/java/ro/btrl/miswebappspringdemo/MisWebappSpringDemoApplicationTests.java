750fe234aa9044656889f0f17c5040b0631b4b58
marius1.pop <marius1.pop@btrl.ro>
2021-11-11 08:23:30 +0200

    add jaccoco plugin for test coverage

--- src/test/java/ro/btrl/miswebappspringdemo/MisWebappSpringDemoApplicationTests.java
+++ src/test/java/ro/btrl/miswebappspringdemo/MisWebappSpringDemoApplicationTests.java
@@ -3,7 +3,6 @@ package ro.btrl.miswebappspringdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MisWebappSpringDemoApplicationTests {
 
 	@Test
